2cef23ec7323fa556ccb46d32b761faa12dbaf2c
DariusSohWX <142966037+DariusSohWX@users.noreply.github.com>
2023-08-30 18:15:42 +0800

    backend

---
+++ jio/src/main/java/G3/jio/JioApplication.java
@@ -0,0 +1,13 @@
+package G3.jio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JioApplication.class, args);
+	}
+
+}
---
+++ jio/src/test/java/G3/jio/JioApplicationTests.java
@@ -0,0 +1,13 @@
+package G3.jio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
