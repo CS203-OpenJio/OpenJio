61cee90debd706413b05864d504c791e9fed5508
ignatius <142966847+ign626@users.noreply.github.com>
2023-09-06 23:48:50 +0800

    Add events and category enity

---
+++ jio/src/main/java/G3/jio/entities/Category.java
@@ -0,0 +1,22 @@
+package G3.jio.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "category")
+public class Category {
+    @Id
+    @Column(name ="Category name")
+    private String name;
+}
---
+++ jio/src/main/java/G3/jio/entities/Events.java
@@ -0,0 +1,58 @@
+package G3.jio.entities;
+import java.util.List;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "events")
+
+public class Events {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO);
+    private int eventID;
+
+    @Column(name = "Event name")
+    private String event_name;
+
+    @Column(name = "startDate")
+    private LocalDate startDate;
+
+    @Column(name = "endDate")
+    private LocalDate endDate;
+
+    @Column(name = "venue")
+    private String venue ;
+
+    @Column(name = "capacity")
+    private int capacity;
+
+    @Column(name = "isRegistered")
+    private boolean isRegistered;
+
+    @Column(name = "algo type")
+    private int algo;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "isVisible")
+    private boolean isVisible;
+
+
+    private List<User> listOfusers;
+
+}
