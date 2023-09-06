624f4067db22bb254f971cff94ddc0a682f13fdf
DariusSohWX <142966037+DariusSohWX@users.noreply.github.com>
2023-09-06 23:40:05 +0800

    Update Account.java

--- jio/src/main/java/G3/jio/entities/Account.java
+++ jio/src/main/java/G3/jio/entities/Account.java
@@ -1,5 +1,38 @@
 package G3.jio.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "accounts")
 public class Account {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long accId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "image")
+    private String image;
+
+    @Column(name = "accType")
+    private char accType;
 }
