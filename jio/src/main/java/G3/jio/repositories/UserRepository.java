e6629aaa6a63277c4b66666a7231e02466f5269c
DariusSohWX <142966037+DariusSohWX@users.noreply.github.com>
2023-09-07 18:41:54 +0800

    Update UserRepository.java

--- jio/src/main/java/G3/jio/repositories/UserRepository.java
+++ jio/src/main/java/G3/jio/repositories/UserRepository.java
@@ -3,11 +3,9 @@ package G3.jio.repositories;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import G3.jio.entities.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     public List<User> findAllByName();
