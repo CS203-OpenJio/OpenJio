b98e9d5740fa9412d3fb0d9bc56f1057a2c689b1
whatron <94902703+whatron@users.noreply.github.com>
2023-09-16 00:40:56 +0800

    Updated Several Pages
    Updated Event Page, Created Ticket Purchased Page, created new routes, added comments in app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createContext } from "react";
 import HomeScreen from "./pages/HomeScreen";
 import CentralHub from "./pages/CentralHub";
 import LoginPage from "./pages/LoginPage";
 import EventPage from "./pages/EventPage";
+import TicketPurchased from "./pages/TicketPurchased";
 // import { Route } from "react-router-dom";
 
 // create the configuration for a router by simply passing
@@ -20,14 +22,26 @@ const router = createBrowserRouter([
     path: "/login",
     element: <LoginPage />,
   },
+  // might need to update below as they require data passed through them for them to load
   {
     path: "/eventpage",
     element: <EventPage />,
   },
+  {
+    path: "/purchased",
+    element: <TicketPurchased />,
+  },
 ]);
 
+// Contexts allows components to pass information down without explicitly passing props
+// We can pass light/dark mode states through these, and user info as well?
+const ThemeContext = createContext('light');
+const AuthContext = createContext(null);
+
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+        <RouterProvider router={router} />
+  );
 };
 
 export default App;
--- frontend/src/pages/EventPage.tsx
+++ frontend/src/pages/EventPage.tsx
@@ -1,9 +1,43 @@
+import TicketFooter from "../components/EventPage/TicketFooter";
 import NavBar from "../components/HomeScreen/NavBar";
+import { useState, useEffect } from "react";
 
-export default function EventPage(){
+export default function EventPage() {
+
+    //Sends GET request and updates posts
+    const [postData, setPostData] = useState([] as any[]);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                setPostData(data);
+            })
+            .catch((err) => {
+                console.log(err.message);
+            });
+    }, []);
+
+
     return (
-    <div>
-        <NavBar />
-    </div>
+        <div>
+            <NavBar />
+            <div className="h-20"></div>
+            <TicketFooter />
+            <div>
+                {/* Maps post data! */}
+                {postData.map((post) => {
+                    return (
+                        <div className="post-card" key={post.id}>
+                            <h2 className="post-title">{post.title}</h2>
+                            <p className="post-body">{post.body}</p>
+                            <div className="button">
+                                <div className="delete-btn">Delete</div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
     );
 };
