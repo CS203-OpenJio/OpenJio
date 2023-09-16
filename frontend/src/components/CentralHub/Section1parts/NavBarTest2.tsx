39d9b591737c7cb301a3f4df8db33c6ec6e509fc
Ashley Tan <ashleyytanjx@gmail.com>
2023-09-16 13:13:57 +0800

    Updated NavBar for CentralHub for consistency

---
+++ frontend/src/components/CentralHub/Section1parts/NavBarTest2.tsx
@@ -0,0 +1,77 @@
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+function NavBarTest2() {
+  const navigate = useNavigate();
+
+  const handleClick = (path: string) => {
+    navigate(path);
+  };
+
+  const scrollToElement = (scrollTarget: string) => {
+    const targetElement = document.getElementById(scrollTarget);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  //For NavBar transparency effect
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 20) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <div
+      className={`top-0 flex justify-between items-center fixed w-full ${
+        isScrolled ? "bg-white shadow-sm" : ""
+      } z-50 pt-2 pb-2`}
+      style={{
+        transition: "background-color 0.2s ease-in-out",
+      }}
+    >
+      <img
+        className="object-cover h-12 ml-5 cursor-pointer"
+        alt="OpenJio Logo"
+        src="/logo.png"
+      />
+      <div className="font-medium flex flex-row text-center text-3xl font-ibm-plex-mono w-1/3 justify-between cursor-pointer">
+        <div
+          onClick={() => scrollToElement("scrollOpenJio")}
+          className="hover:bg-black hover:text-white"
+        >
+          OpenJio?
+        </div>
+        <div
+          onClick={() => scrollToElement("scrollAboutUs")}
+          className="hover:bg-black hover:text-white"
+        >
+          About Us
+        </div>
+        <div className="hover:bg-black hover:text-white">FAQ</div>
+      </div>
+      <div className="mr-5" onClick={() => handleClick("/logout")}>
+        <img
+          className="object-cover mr-11 cursor-pointer"
+          alt="Logout button"
+          src="/logout.png"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default NavBarTest2;
--- frontend/src/pages/CentralHub.tsx
+++ frontend/src/pages/CentralHub.tsx
@@ -1,17 +1,19 @@
 import NavBarCentralHub from "../components/CentralHub/Section1parts/NavBarCentralHub";
 import Section1 from "../components/CentralHub/Section1";
 import Section2 from "../components/CentralHub/Section2";
+import NavBarTest2 from "../components/CentralHub/Section1parts/NavBarTest2";
 
 function CentralHub() {
   return (
     <div>
-      <NavBarCentralHub />
+      <NavBarTest2 />
       <Section1 />
       <p></p>
       <div className="pb-3 ml-14 font-source-serif-pro text-darkslateblue text-31xl [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)">
         Popular & Trending
       </div>
       <Section2 />
+      <p></p>
     </div>
   );
 }
