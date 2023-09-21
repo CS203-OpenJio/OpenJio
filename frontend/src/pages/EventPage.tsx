68eaf98d2f18b2ab9306eb556b52199f73742459
whatron <94902703+whatron@users.noreply.github.com>
2023-09-21 14:00:36 +0800

    Updated EventPage
    UI changes

--- frontend/src/pages/EventPage.tsx
+++ frontend/src/pages/EventPage.tsx
@@ -1,12 +1,13 @@
+import { Link } from "react-router-dom";
 import NavBarTest2 from "../components/CentralHub/Section1parts/NavBarTest2";
-import TicketFooter from "../components/EventPage/TicketFooter";
 import { useState, useEffect } from "react";
+
 
 export default function EventPage() {
   //Sends GET request and updates posts
   const userName = 'admin';
   const password = 'admin';
-  
+
   const [postData, setPostData] = useState([] as any[]);
   const options = {
     method: 'GET',
@@ -15,6 +16,17 @@ export default function EventPage() {
     },
   };
 
+  function TicketFooter() {
+    return (
+      <div>
+        <Link to="/purchased">
+          <div className="button">
+            <div className="bg-black rounded-xl text-white hover:bg-white hover:text-black shadow-md cursor-pointer">Sign Up!</div>
+          </div>
+        </Link>
+      </div>
+    );
+  }
 
   useEffect(() => {
     fetch("http://localhost:8080/api/v1/events", options)
@@ -33,17 +45,17 @@ export default function EventPage() {
     <div>
       <NavBarTest2 />
       <div className="h-20"></div>
-      <TicketFooter />
-      <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-10 w-[60%] m-auto">
         {/* Maps post data! */}
         {postData.map((post) => {
           return (
-            <div className="post-card" key={post.id}>
-              <h2 className="post-title">{post.description}</h2>
-              <p className="post-body">{post.body}</p>
-              <div className="button">
-                <div className="delete-btn">Delete</div>
-              </div>
+            <div className="m-auto overflow-hidden text-center text-xl font-ibm-plex-mono" key={post.id}>
+              <h2 className="post-title">{post.name}</h2>
+              <p className="post-body">Description: {post.description}</p>
+              <p className="post-body">From {post.startDate} to {post.endDate}</p>
+              <p className="post-body">Venue at : {post.venue}</p>
+              <p className="post-body">Event capacity: {post.capacity}</p>
+              <TicketFooter />
             </div>
           );
         })}
