ae00c04c9b26c91431c550c2133b08f4fbd638c6
Shinkeishi <ashleyytanjx@gmail.com>
2023-09-22 04:07:06 +0800

    Fixed logout page and added to Event Page

--- frontend/src/pages/LogoutPage.tsx
+++ frontend/src/pages/LogoutPage.tsx
@@ -11,15 +11,17 @@ const LogoutPage = () => {
   return (
     <>
       <LogoutNavBar />
-      <div className="mt-[240px] font-medium text-31xl font-ibm-plex-mono text-center">
-        You have successfully logged out!
+      <div className="flex flex-col justify-normal items-center">
+        <div className="mt-[240px] font-medium text-31xl font-ibm-plex-mono text-center">
+          You have successfully logged out!
+        </div>
+        <button
+          className="w-[400px] mt-10 bg-white hover:translate hover:bg-black hover:text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow text-4xl font-ibm-plex-mono cursor-pointer transform active:scale-75 transition-transform"
+          onClick={() => handleClick("/")}
+        >
+          Back to Homepage
+        </button>
       </div>
-      <button
-        className="mt-10 bg-white hover:translate hover:bg-black hover:text-white font-semibold py-2 px-4 border border-gray-400 rounded shadow text-4xl font-ibm-plex-mono cursor-pointer transform active:scale-75 transition-transform"
-        onClick={() => handleClick("/")}
-      >
-        Back to Homepage
-      </button>
     </>
   );
 };
