b7a37018787fba3ffb3c31d2f1dde7087b2154aa
whatron <94902703+whatron@users.noreply.github.com>
2023-09-21 15:14:59 +0800

    Update EventPage.tsx
    Added temporary onpage event adder

--- frontend/src/pages/EventPage.tsx
+++ frontend/src/pages/EventPage.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import NavBarTest2 from "../components/CentralHub/Section1parts/NavBarTest2";
 import { useState, useEffect } from "react";
+import axios from "axios";
 
 
 export default function EventPage() {
@@ -16,18 +17,6 @@ export default function EventPage() {
     },
   };
 
-  function TicketFooter() {
-    return (
-      <div>
-        <Link to="/purchased">
-          <div className="button">
-            <div className="bg-black rounded-xl text-white hover:bg-white hover:text-black shadow-md cursor-pointer">Sign Up!</div>
-          </div>
-        </Link>
-      </div>
-    );
-  }
-
   useEffect(() => {
     fetch("http://localhost:8080/api/v1/events", options)
       .then((response) => response.json())
@@ -40,11 +29,36 @@ export default function EventPage() {
       });
   }, []);
 
+  async function handleClick() {
+
+    const body = {
+      "name": `CS201`,
+      "startDate": "2001-02-04",
+      "endDate": "2001-08-10",
+      "description": "Data Structures and Algorithm",
+      "capacity": 45,
+      "eventType": "Workshop",
+      "venue": "venue",
+      "registered": false,
+      "visible": false
+    }
+    await axios.post("http://localhost:8080/api/v1/events", body, {
+      headers: {
+        'Authorization': 'Basic ' + btoa(`${userName}:${password}`),
+      }
+    })
+      .catch((err) => {
+        console.log(err.message);
+      });
+    window.location.reload();
+  }
+
   return (
     <div>
       <NavBarTest2 />
       <div className="h-20"></div>
       <div className="flex flex-col gap-10 w-[60%] m-auto">
+      <button className="m-auto cursor-pointer bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={handleClick}>Add Event</button>
         {/* Maps post data! */}
         {postData.map((post) => {
           return (
@@ -61,4 +75,17 @@ export default function EventPage() {
       </div>
     </div>
   );
+  function TicketFooter() {
+    return (
+      <div>
+        <Link to="/purchased">
+          <div className="button">
+            <div className="bg-black rounded-xl text-white hover:bg-white hover:text-black shadow-md cursor-pointer">Sign Up!</div>
+          </div>
+        </Link>
+      </div>
+    );
+  }
+
+
 }
