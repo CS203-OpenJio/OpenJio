8091a3b3766d1cc76723ec63de8098a4cdec58ee
Justin Teo <justin.teo.2022@scis.smu.edu.sg>
2023-09-26 21:11:16 +0800

    Added Role.java and Privilege.java

---
+++ jio/src/main/java/G3/jio/entities/Privilege.java
@@ -0,0 +1,22 @@
+package G3.jio.entities;
+
+import java.util.Collection;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+@Entity
+public class Privilege {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(mappedBy = "privileges")
+    private Collection<Role> roles;
+}
