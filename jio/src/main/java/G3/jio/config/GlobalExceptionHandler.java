767e59ba3a92843bdd442adefd8f0394ae3fab3c
DariusSohWX <142966037+DariusSohWX@users.noreply.github.com>
2023-09-26 16:54:20 +0800

    Fixed Errorhandling

--- jio/src/main/java/G3/jio/config/GlobalExceptionHandler.java
+++ jio/src/main/java/G3/jio/config/GlobalExceptionHandler.java
@@ -1,16 +1,21 @@
 package G3.jio.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -21,18 +26,48 @@ import G3.jio.exceptions.NotExistException;
 import G3.jio.exceptions.UserNotFoundException;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
-@RestControllerAdvice
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     // Ensure all inputs are valid and follows set constraints
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String defaultErrorMsg = ex.getAllErrors().get(0).getDefaultMessage();
+    // @Override
+    // protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    //     String defaultErrorMsg = ex.getAllErrors().get(0).getDefaultMessage();
 
-        ErrorModel error = new ErrorModel(HttpStatus.BAD_REQUEST, "Validation Error: " + defaultErrorMsg,
-                ex.getBindingResult().toString());
-        return new ResponseEntity<Object>(error, HttpStatus.BAD_REQUEST);
-    }
+    //     ErrorModel error = new ErrorModel(HttpStatus.BAD_REQUEST, "Validation Error: " + defaultErrorMsg,
+    //             ex.getBindingResult().toString());
+    //     return new ResponseEntity<Object>(error, HttpStatus.BAD_REQUEST);
+    // }
+
+
+
+    	/**
+	 * Customize the handling of {@link MethodArgumentNotValidException}.
+	 * <p>This method delegates to {@link #handleExceptionInternal}.
+	 * @param ex the exception to handle
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} for the response to use, possibly
+	 * {@code null} when the response is already committed
+	 */
+    @Override
+	// @Nullable
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+                logger.info(ex.getClass().getName());
+                //
+                final List<String> errors = new ArrayList<String>();
+                for (final FieldError error : ex.getBindingResult().getFieldErrors()) {
+                    errors.add(error.getField() + ": " + error.getDefaultMessage());
+                }
+                for (final ObjectError error : ex.getBindingResult().getGlobalErrors()) {
+                    errors.add(error.getObjectName() + ": " + error.getDefaultMessage());
+                }
+                final ApiError apiError = new ApiError(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), errors);
+                return handleExceptionInternal(ex, apiError, headers, apiError.getStatus(), request);
+	}
 
     @ExceptionHandler(AuthenticationException.class)
     private ResponseEntity<ErrorModel> handleAuthentication(AuthenticationException ex) {
