f76e56e41024737108221eaa5dc96cfa5e45aaf7
Ignatius Goh Lie Kai <ignatiusgoh.2022@scis.smu.edu.sg>
2023-10-10 04:44:28 +0800

    Added fileData and imageData entities

---
+++ jio/src/main/java/G3/jio/entities/FileData.java
@@ -0,0 +1,26 @@
+package G3.jio.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "FILE_DATA")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class FileData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    private String filePath;
+}
---
+++ jio/src/main/java/G3/jio/entities/ImageData.java
@@ -0,0 +1,35 @@
+package G3.jio.entities;
+
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "imagedata")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+
+public class ImageData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String type;
+    @Lob
+    @Column(name = "imagedata",length = 1000)
+    private byte[] imageData;
+}
