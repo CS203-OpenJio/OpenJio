c8b6281370bf2e089b607bf0b46070caca930e4a
Justin Teo <justintyf@hotmail.com>
2023-10-18 02:06:51 +0800

    Moved sensitive information to .env file

--- jio/src/main/java/G3/jio/JioApplication.java
+++ jio/src/main/java/G3/jio/JioApplication.java
@@ -1,10 +1,15 @@
 package G3.jio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:.env")
 public class JioApplication {
 
         public static void main(String[] args) {
