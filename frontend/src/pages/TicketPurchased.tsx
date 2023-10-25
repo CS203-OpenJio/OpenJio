1448765cd2420a01c477593436e54032e38f6a1f
whatron <94902703+whatron@users.noreply.github.com>
2023-10-26 00:29:33 +0800

    Fixed Wrong NavBar
    Page was using wrong navbar

--- frontend/src/pages/TicketPurchased.tsx
+++ frontend/src/pages/TicketPurchased.tsx
@@ -1,4 +1,4 @@
-import NavBarLite from "../components/HomeScreen/NavBarLite";
+import NavBar from "../components/NavBar";
 import { Link, useLocation } from "react-router-dom";
 
 export default function TicketPurchased() {
@@ -7,7 +7,7 @@ export default function TicketPurchased() {
 
   return (
     <div>
-      <NavBarLite />
+      <NavBar />
       <div className="h-screen">
         <div className="h-1"></div>
         <div className="flex justify-center mt-[10vw]">
