ac76744e271887e56105f71a6471026618668810
DariusSohWX <142966037+DariusSohWX@users.noreply.github.com>
2023-10-27 00:19:33 +0800

    fixed updateEvent method

--- jio/src/main/java/G3/jio/services/EventService.java
+++ jio/src/main/java/G3/jio/services/EventService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
@@ -99,7 +100,9 @@ public class EventService {
         }
         
         ModelMapper mapper = new ModelMapper();
-        mapper.getConfiguration().setSkipNullEnabled(true);
+        mapper.getConfiguration()
+            .setSkipNullEnabled(true)
+            .setMatchingStrategy(MatchingStrategies.STRICT);
         mapper.map(eventDTO, event);
         eventRepository.saveAndFlush(event);
         
