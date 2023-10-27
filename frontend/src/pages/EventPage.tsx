9f2cd150844f6b08835f251a06e6aa5b33899104
Pramit008 <pramits.2022@scis.smu.edu.sg>
2023-10-28 05:40:57 +0800

    made event page a lot fancier....

--- frontend/src/pages/EventPage.tsx
+++ frontend/src/pages/EventPage.tsx
@@ -38,32 +38,44 @@ export default function EventPage() {
 
  
   return (
-    <div className="bg-gradient-to-br from-gray-100 to-gray-200 min-h-screen flex flex-col justify-center">
+    <div className="bg-gradient-to-br from-gray-100 to-gray-200 min-h-screen flex flex-col justify-center font-poppins">
       <NavBar />
       <div className="container mx-auto px-4 py-10">
-        <div className="bg-white rounded-xl p-8 shadow-xl mx-auto border-2 border-gray-300 w-full max-w-2xl" key={event?.id}>
-          <h2 className="text-4xl font-semibold mb-8 text-center text-gray-700">{event?.name}</h2>
-          <div className="flex justify-center mb-8">
+        <div className="bg-white rounded-xl p-8 shadow-2xl mx-auto border-4 border-gray-300 w-full max-w-2xl" key={event?.id}>
+          
+          <h2 className="text-4xl font-extrabold mb-10 text-center text-gray-700 leading-tight">{event?.name}</h2>
+          
+          <div className="flex justify-center mb-10 relative group">
             <img
               src={event?.image}
               alt={event?.name}
-              className="h-[400px] w-[300px] rounded-3xl shadow-md"
+              className="h-[400px] w-[300px] rounded-3xl shadow-lg transform transition-transform duration-300 group-hover:scale-105"
             />
+           
           </div>
-          <div className="mb-8 text-center">
-            <p className="text-lg mb-4">
-              <span className="font-semibold">Date:</span> {new Date(event?.startDateTime).toLocaleString()} to {new Date(event?.endDateTime).toLocaleString()}
-            </p>
-            <p className="text-lg mb-4">
-              <span className="font-semibold">Venue:</span> {event?.venue}
-            </p>
-            <p className="text-lg mb-4">
-              <span className="font-semibold">Max Event Capacity:</span> {event?.capacity}
-            </p>
+          
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-10">
+            <div className="p-6 bg-gray-100 rounded-lg text-center shadow-lg transform transition-transform duration-300 hover:scale-105">
+              <div className="font-semibold text-xl mb-2">Date</div>
+              {new Date(event?.startDateTime).toLocaleDateString()} to {new Date(event?.endDateTime).toLocaleDateString()}
+            </div>
+            <div className="p-6 bg-gray-100 rounded-lg text-center shadow-lg transform transition-transform duration-300 hover:scale-105">
+              <div className="font-semibold text-xl mb-2">Time</div>
+              {new Date(event?.startDateTime).toLocaleTimeString()} to {new Date(event?.endDateTime).toLocaleTimeString()}
+            </div>
+            <div className="p-6 bg-gray-100 rounded-lg text-center shadow-lg transform transition-transform duration-300 hover:scale-105">
+              <div className="font-semibold text-xl mb-2">Venue</div>
+              {event?.venue}
+            </div>
           </div>
-          <div className="text-gray-700 mb-8 text-center">
-            {event?.description}
+          
+          <div className="bg-white p-6 rounded-lg shadow-lg border-2 border-gray-200 mb-10 transform transition-transform duration-300 hover:scale-105">
+            <div className="font-semibold text-2xl mb-4 text-center">Event Description</div>
+            <div className="text-gray-700 text-center leading-relaxed">
+              {event?.description}
+            </div>
           </div>
+
           <div className="flex justify-center">
             <TicketFooter id={event?.id} />
           </div>
