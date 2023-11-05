7f024137cfe4ffbc4527870de03c7ad5e6f8a402
Shinkeishi <ashleyytanjx@gmail.com>
2023-11-05 20:37:58 +0800

    Update Swagger.tsx

--- frontend/src/pages/Swagger.tsx
+++ frontend/src/pages/Swagger.tsx
@@ -3,7 +3,7 @@ import "swagger-ui-react/swagger-ui.css";
 const Swagger = () => {
   return (
     <iframe
-      src="https://openjio-loadbalancer-backend-824860381.ap-southeast-1.elb.amazonaws.com/swagger-ui.html"
+      src="http://openjio-loadbalancer-backend-824860381.ap-southeast-1.elb.amazonaws.com/swagger-ui.html"
       width="800"
       height="600"
     ></iframe>
