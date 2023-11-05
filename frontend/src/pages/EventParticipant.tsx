159a11961ab62e22d4e317fe8c8d19cb78100516
whatron <94902703+whatron@users.noreply.github.com>
2023-11-05 23:31:10 +0800

    Updated Edit Participants
    Algo select on event participant view

--- frontend/src/pages/EventParticipant.tsx
+++ frontend/src/pages/EventParticipant.tsx
@@ -7,7 +7,7 @@ import {
     TableHeader,
     TableRow,
 } from "src/components/ui/table";
-import { Input, Button, ToggleButtonGroup, ToggleButton, ButtonGroup } from '@mui/material';
+import { Input, Button, ToggleButtonGroup, ToggleButton, ButtonGroup, Tooltip } from '@mui/material';
 import { CSVLink } from "react-csv";
 import NavBar from '../components/NavBar';
 import { getParticipants, getStatusParticipants, allocateSlots, closeEvent } from '../utils/CreatedEventController';
@@ -15,6 +15,21 @@ import { getEvents } from '../utils/EditEventController';
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { toast } from 'react-toastify';
 import CloudDownloadIcon from '@mui/icons-material/CloudDownload';
+import { Check, ChevronsUpDown } from "lucide-react"
+import { cn } from "../lib/utils"
+import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+} from "../components/ui/command"
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "../components/ui/popover"
+import { string } from 'zod';
 
 interface Participant {
     id: number;
@@ -22,11 +37,13 @@ interface Participant {
     email: string;
     phone: string;
     matricNo: string;
+    smuCreditScore: string;
 }
 
 const participantsData: Participant[] = [
-    { id: 1, name: 'no one found', email: 'no one found', phone: 'no one found', matricNo: 'no one found' },
+    { id: 1, name: 'no one found', email: 'no one found', phone: 'no one found', matricNo: 'no one found', smuCreditScore: "no one" },
 ];
+
 
 const EventParticipant = () => {
     const [searchParams] = useSearchParams();
@@ -37,6 +54,8 @@ const EventParticipant = () => {
     const [alignment, setAlignment] = React.useState('web');
     const [completed, setCompleted] = useState(false);
     const [eventName, setEventName] = useState("");
+    const [value, setValue] = useState<string | null>(null);
+
 
     const handleChange = async (
         event: React.MouseEvent<HTMLElement>,
@@ -104,12 +123,12 @@ const EventParticipant = () => {
     const handleAllocate = async () => {
         if (eventId) {
             try {
-                await allocateSlots(eventId);
+                await allocateSlots(eventId, value);
                 await getStatusParticipants(eventId, "ACCEPTED").then((eventParticipants) => {
                     setParticipants(eventParticipants);
                     handleChange({} as React.MouseEvent<HTMLElement>, 'ACCEPTED');
                 });
-            } catch (error:any) {
+            } catch (error: any) {
                 console.log(error)
                 toast.error(error.message);
             }
@@ -166,9 +185,9 @@ const EventParticipant = () => {
     async function handleClose(): Promise<void> {
         if (eventId) {
             try {
-            await closeEvent(eventId)
-            toast.success("Event closed.");
-            updateCompleted();
+                await closeEvent(eventId)
+                toast.success("Event closed.");
+                updateCompleted();
             } catch (error) {
                 toast.error("Error closing event.");
             }
@@ -183,7 +202,7 @@ const EventParticipant = () => {
             <div className="container mx-auto max-w-4xl mt-[80px]">
                 <div className="py-8">
                     <h2 className="text-2xl leading-tight font-ibm-plex-mono">Participants<span className='text-sm'> of <span className='underline tracking-tight'>{eventName}</span></span></h2>
-                    <div className="flex flex-row mb-1 sm:mb-0 justify-between h-8">
+                    <div className="flex flex-row mb-1 sm:mb-0 justify-between">
                         <div className='flex-col'>
                             <ToggleButtonGroup
                                 className='h-8'
@@ -201,7 +220,7 @@ const EventParticipant = () => {
                             <Input placeholder="Search" className="ml-2" value={searchTerm} onChange={handleSearch} />
                         </div>
 
-                        <div className='flex'>
+                        <div className='flex flex-col items-end'>
                             <ButtonGroup variant="contained" aria-label="outlined primary button group">
                                 <Button
                                     className='bg-green-500'
@@ -209,9 +228,58 @@ const EventParticipant = () => {
                                     <CSVLink {...handleExport()} filename={'participants.csv'} className="text-white no-underline flex">
                                         <CloudDownloadIcon /><span className='ml-1'>.CSV</span>
                                     </CSVLink>
+
                                 </Button>
-                                <Button onClick={handleAllocate} disabled={completed}>Allocate</Button>
                                 <Button onClick={handleClose} disabled={completed} color="error">{completed ? "event closed" : "close event"}</Button>
+                            </ButtonGroup>
+                            <ButtonGroup variant="contained" aria-label="outlined primary button group" >
+                                <Popover>
+                                    <PopoverTrigger asChild>
+                                        <Button
+                                            role="combobox"
+                                            className={cn(
+                                                "w-[200px] justify-between hover:cursor-pointer",
+                                                !value && "text-muted-foreground"
+                                            )}
+                                        >
+                                            {value
+                                                ? languages.find(
+                                                    (language) => language.value === value
+                                                )?.label
+                                                : "Select Algorithm"}
+                                            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                        </Button>
+                                    </PopoverTrigger>
+                                    <PopoverContent className="w-[300px] p-0 mt-[-4px]">
+                                        <Command>
+                                            <CommandGroup>
+                                                {languages.map((language) => (
+                                                    <Tooltip title={language.desc} placement="right">
+                                                        <CommandItem
+                                                            value={language.label}
+                                                            className="font-ibm-plex-mono hover:cursor-pointer"
+                                                            key={language.value}
+                                                            onSelect={() => {
+                                                                setValue(language.value)
+                                                            }}
+                                                        >
+                                                            <Check
+                                                                className={cn(
+                                                                    "mr-2 h-4 w-4",
+                                                                    language.value === value
+                                                                        ? "opacity-100"
+                                                                        : "opacity-0"
+                                                                )}
+                                                            />
+                                                            {language.label}
+                                                        </CommandItem>
+                                                    </Tooltip>
+                                                ))}
+                                            </CommandGroup>
+                                        </Command>
+                                    </PopoverContent>
+                                </Popover>
+                                <Button onClick={handleAllocate} disabled={completed}>Allocate</Button>
                             </ButtonGroup>
                         </div>
 
@@ -226,7 +294,8 @@ const EventParticipant = () => {
                                     <TableHead className="hover:cursor-pointer" onClick={() => handleSort('name')}>Name</TableHead>
                                     <TableHead className="hover:cursor-pointer" onClick={() => handleSort('email')}>Email</TableHead>
                                     <TableHead className="hover:cursor-pointer" onClick={() => handleSort('phone')}>Phone</TableHead>
-                                    <TableHead className="hover:cursor-pointer" onClick={() => handleSort('phone')}>Matric No.</TableHead>
+                                    <TableHead className="hover:cursor-pointer" >Matric No.</TableHead>
+                                    <TableHead className="hover:cursor-pointer" onClick={() => handleSort('smuCreditScore')}>Student Score</TableHead>
                                 </TableRow>
                             </TableHeader>
                             <TableBody>
@@ -237,6 +306,7 @@ const EventParticipant = () => {
                                         <TableCell>{participant.email}</TableCell>
                                         <TableCell>{participant.phone}</TableCell>
                                         <TableCell>{participant.matricNo}</TableCell>
+                                        <TableCell>{participant.smuCreditScore}</TableCell>
                                     </TableRow>
                                 ))}
                             </TableBody>
@@ -250,3 +320,10 @@ const EventParticipant = () => {
 };
 
 export default EventParticipant;
+
+const languages = [
+    { label: "Normal Queue", value: "FCFS", desc: "First Come First Serve" },
+    { label: "Random", value: "Random", desc: "Participants are randomly chosen" },
+    { label: "Priority Queue", value: "Score", desc: "Participants are chosen from a queue, but weighted by their event attendance rate" },
+    { label: "Weighted Random", value: "Weighted Random", desc: "Participants are chosen randomly, participants with a history of successfully attending events have a higher chance of being chosen" },
+] as const
