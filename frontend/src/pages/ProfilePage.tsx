b3ea148698342085fb711f938ab694f7c316ef64
Pramit008 <pramits.2022@scis.smu.edu.sg>
2023-11-06 16:09:31 +0800

    pushed profile page w style more aligned w rest of pages(need test)
    ...

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -57,62 +57,63 @@ const ChangeProfile: FunctionComponent = () => {
     }
 
 
+
     return (
-        <div className="h-screen bg-floralwhite text-darkslateblue font-ibm-plex-mono">
+        <div className="flex flex-col h-screen">
             <NavBar />
-            <div className="flex justify-center items-center h-full">
-                <div className="bg-white p-8 rounded-xl shadow-xl w-full max-w-md border-4 border-darkslateblue">
-                    <h2 className="text-2xl mb-6 text-center text-darkslateblue font-semibold">
-                        {isEditing ? "Change Profile Details" : "Profile Details"}
-                    </h2>
-                    {isEditing ? (
-                        <div className="flex flex-col items-center">
-                            <input
-                                className="w-4/5 p-2 mb-4 border rounded-xl focus:ring focus:ring-darkslateblue transition ease-in-out border-darkslateblue"
-                                type="date"
-                                placeholder="Date of Birth"
-                                value={dob}
-                                onChange={(e) => setDob(e.target.value)}
-                            />
-                            <input
-                                className="w-4/5 p-2 mb-4 border rounded-xl focus:ring focus:ring-darkslateblue transition ease-in-out border-darkslateblue"
-                                type="text"
-                                placeholder="Matric No."
-                                value={matricNo}
-                                onChange={(e) => setMatricNo(e.target.value)}
-                            />
-                            <input
-                                className="w-4/5 p-2 mb-4 border rounded-xl focus:ring focus:ring-darkslateblue transition ease-in-out border-darkslateblue"
-                                type="text"
-                                placeholder="Phone"
-                                value={phone}
-                                onChange={(e) => setPhone(e.target.value)}
-                            />
-                            <button
-                                className="w-4/5 p-2 bg-darkslateblue text-white rounded-xl hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-darkslateblue"
-                                onClick={() => setDetails(studentId, matricNo, phone, dob)}
-                            >
-                                Update Profile
-                            </button>
-                        </div>
-                    ) : (
-                        <>
-                            <div className="mb-4 text-darkslateblue font-medium">Date of Birth: <span className="text-black">{dob}</span></div>
-                            <div className="mb-4 text-darkslateblue font-medium">Matriculation Number: <span className="text-black">{matricNo}</span></div>
-                            <div className="mb-4 text-darkslateblue font-medium">Phone: <span className="text-black">{phone}</span></div>
-                            <button
-                                className="w-full p-2 bg-darkslateblue text-white rounded-xl hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-darkslateblue"
-                                onClick={() => setIsEditing(true)}
-                            >
-                                Edit Profile
-                            </button>
-                        </>
-                    )}
-                    <div className="flex flex-row items-center mt-12">
-                        <div>
-                        Student Score:
+            <div className="flex-1 relative bg-floralwhite w-full overflow-hidden text-left text-base text-black font-ibm-plex-mono">
+                <div className="flex flex-col justify-center items-center h-full">
+                    <div className="bg-white p-8 rounded-xl shadow-xl w-full max-w-md border-4 border-darkslateblue">
+                        <h2 className="text-2xl mb-6 text-center text-darkslateblue font-semibold">
+                            {isEditing ? "Change Profile Details" : "Profile Details"}
+                        </h2>
+                        {isEditing ? (
+                            <div className="flex flex-col items-center">
+                                <input
+                                    className="w-4/5 p-2 mb-4 rounded-xl focus:ring focus:ring-darkslateblue transition ease-in-out border border-darkslateblue"
+                                    type="date"
+                                    placeholder="Date of Birth"
+                                    value={dob}
+                                    onChange={(e) => setDob(e.target.value)}
+                                />
+                                <input
+                                    className="w-4/5 p-2 mb-4 rounded-xl focus:ring focus:ring-darkslateblue transition ease-in-out border border-darkslateblue"
+                                    type="text"
+                                    placeholder="Matric No."
+                                    value={matricNo}
+                                    onChange={(e) => setMatricNo(e.target.value)}
+                                />
+                                <input
+                                    className="w-4/5 p-2 mb-4 rounded-xl focus:ring focus:ring-darkslateblue transition ease-in-out border border-darkslateblue"
+                                    type="text"
+                                    placeholder="Phone"
+                                    value={phone}
+                                    onChange={(e) => setPhone(e.target.value)}
+                                />
+                                <button
+                                    className="w-4/5 p-2 bg-darkslateblue text-white rounded-xl hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-darkslateblue transition-transform duration-100 ease-in-out transform active:scale-95"
+                                    onClick={() => setDetails(studentId, matricNo, phone, dob)}
+                                >
+                                    Update Profile
+                                </button>
                             </div>
-                    <Progress value={score} className="w-[40%]" />
+                        ) : (
+                            <>
+                                <div className="mb-4 text-darkslateblue font-medium">Date of Birth: <span className="text-black">{dob}</span></div>
+                                <div className="mb-4 text-darkslateblue font-medium">Matriculation Number: <span className="text-black">{matricNo}</span></div>
+                                <div className="mb-4 text-darkslateblue font-medium">Phone: <span className="text-black">{phone}</span></div>
+                                <button
+                                    className="w-full p-2 bg-darkslateblue text-white rounded-xl hover:bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-darkslateblue transition-transform duration-100 ease-in-out transform active:scale-95"
+                                    onClick={() => setIsEditing(true)}
+                                >
+                                    Edit Profile
+                                </button>
+                            </>
+                        )}
+                        <div className="flex flex-row items-center mt-12">
+                            <div>Student Score:</div>
+                            <Progress value={score} className="w-[40%]" />
+                        </div>
                     </div>
                 </div>
             </div>
